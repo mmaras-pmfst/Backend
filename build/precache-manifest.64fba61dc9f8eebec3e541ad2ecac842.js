2ac2f535de6aed5a90a639e2e8113139e3930191
Marko Mihael Maras <mmaras@pmfst.hr>
2020-10-26 10:47:02 +0100

    new

---
+++ build/precache-manifest.64fba61dc9f8eebec3e541ad2ecac842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80305fcb12f40b87e884e84ed0af7b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "067dc73a06df5df7a902",
+    "url": "/static/js/2.2bd4116a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2bd4116a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f4842f9c8660d74267",
+    "url": "/static/js/main.46687512.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app=express();
 const cors=require("cors");
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"))
 
 const zahtjevInfo=(req,res,next)=>{
     console.log("Metoda: ",req.method);
